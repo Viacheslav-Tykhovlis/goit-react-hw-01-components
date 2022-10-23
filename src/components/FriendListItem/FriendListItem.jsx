f949df2a4ef814579e6e0bb34a81720947536a45
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-10-24 00:05:42 +0200

    task-3

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+import { getBackColor } from '../utils/getBackColor';
+
+export const FriendListItem = ({ avatar, name, isOnline, id }) => {
+  return (
+    <li className={css.item} key={id}>
+      <span
+        className={css.status}
+        style={{ backgroundColor: getBackColor(isOnline) }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+  id: PropTypes.number,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
+import { getRandomHexColor } from '../utils/getRandomColor';
 
 export const Statistics = ({ title, stats }) => {
   return (
@@ -9,7 +10,11 @@ export const Statistics = ({ title, stats }) => {
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li className={css.item} key={id}>
+            <li
+              className={css.item}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
               <span className={css.label}>{label}</span>
               <span className={css.percentage}>{percentage}%</span>
             </li>
@@ -22,5 +27,11 @@ export const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.array,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
 };
